edbef6fbec7236477819ca068d30559c678d5bfc
Nisha <nkolivia1234@gmail.com>
2022-12-12 00:31:19 +0530

    e-commerce with MERN

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+       
+        <a href="#" target="_blank">
+         Amazona
+        </a>
+      </header>
+      <main>
+        <p>List of products</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
